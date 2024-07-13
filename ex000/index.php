4274097d10a01b77b9bfb65b933ddb66d9b3d7ed
Matheuslcnh <matheus.cav.san@gmail.com>
2024-07-12 21:24:00 -0300

    create

---
+++ ex000/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro Programa PHP</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo 'Hello World!';
+        ?>
+    </h1>
+    <p>PHP - Primeiro Programa</p>
+</body>
+</html>
